d943a20c10bbe37cd52484087769553842dbe347
Albert P <albert1742@gmail.com>
2022-10-18 11:14:03 +0200

    6.3. JUnit First Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,26 +5,8 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            // Testing SimpleUser class
-            SimpleUser simpleUser = new SimpleUser("theForumUser");
-            String result = simpleUser.getUsername();
 
-            if (result.equals("theForumUser")) {
-                System.out.println("test OK");
-            } else {
-                System.out.println("Error!");
-            }
-
-            // Testing Calculator class
-            Calculator calculator = new Calculator();
-            int addResult = calculator.add(100, 28);
-            int subResult = calculator.subtract(100, 200);
-            if (addResult == 128 && subResult == 100) {
-                System.out.println("Calculator test OK");
-            } else {
-                System.out.println("Calculator has error!");
-            }
-        }
     }
+}
