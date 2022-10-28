9e50aebae354a3261261dd8eafe000cb7b33d394
Albert P <albert1742@gmail.com>
2022-10-28 08:56:45 +0200

    8.2 try! catch! finally!

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,27 +3,28 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    public double divide(double a, double b) throws ArithmeticException  {
-
-        double result = 0;
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-            return  a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Division by 0 not allowed");
-//            throw new ArithmeticException();
-            return 0;
-        } finally {
-            System.out.println("Something that will be done always");
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
     }
+
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
+        double result = 0;
+        try {
+             result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+             result = 0;
+        } finally {
+            System.out.println("The result is : ");
+            System.out.println(result);
+        }
 
-        System.out.println(result);
     }
 }
+
+
+
