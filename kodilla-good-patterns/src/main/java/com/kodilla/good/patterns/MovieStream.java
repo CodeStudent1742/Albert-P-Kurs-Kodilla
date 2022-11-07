aec68613e7cb04165ce02e8d20770118a103c77e
Albert P <albert1742@gmail.com>
2022-11-07 09:17:02 +0100

    13.1 Movie Store

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/MovieStream.java
@@ -0,0 +1,24 @@
+package com.kodilla.good.patterns;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MovieStream {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        String movies = movieStore.getMovies().entrySet().stream()
+                .map(Map.Entry::getValue)
+                .flatMap(s->s.stream())
+                .collect(Collectors.joining(" ! ","",""));
+
+
+        System.out.println(movies);
+    }
+
+
+
+}
